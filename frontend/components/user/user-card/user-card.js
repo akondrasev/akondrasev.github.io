458a88b28ed3f46573c62bfc1e2fa1e1014229c3
akondrasev <anton.kondrasev@live.com>
2017-06-12 21:12:19 +0300

    user card fix path

--- frontend/components/user/user-card/user-card.js
+++ frontend/components/user/user-card/user-card.js
@@ -3,7 +3,7 @@ const userCard = {
     bindings: {
         user: '<'
     },
-    templateUrl: 'components/user/user-card.html',
+    templateUrl: 'components/user/user-card/user-card.html',
     controller: function () {
         const self = this;
         this.onClick = () => self.selected = !self.selected;
