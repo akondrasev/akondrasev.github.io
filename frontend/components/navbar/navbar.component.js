467c33cc8157d16cc77731ab06a96aaae345045e
akondrasev <anton.kondrasev@live.com>
2017-06-18 19:35:11 +0300

    logout

--- frontend/components/navbar/navbar.component.js
+++ frontend/components/navbar/navbar.component.js
@@ -2,7 +2,16 @@ import template from './navbar.html';
 import './navbar.css';
 
 const navbar = {
-    template
+    template,
+    controller: function (authenticationService, $state) {
+        "ngInject";
+
+        this.logout = () => {
+            authenticationService.logout().then(() => {
+                $state.go("login");
+            });
+        }
+    }
 };
 
 export default navbar;
