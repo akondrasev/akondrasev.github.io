e0f5bb87c7b40ea726c9434223c30b18d3b987e4
ankon.kondrasev <anton.kondrasev@live.com>
2017-06-19 10:35:54 +0300

    url https

--- frontend/lib/constants.js
+++ frontend/lib/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-    baseApiUrl: "http://test-api.javascript.ru/v1/anton.kondrasev"
+    baseApiUrl: "https://test-api.javascript.ru/v1/anton.kondrasev"
 };
 
 export default constants;
